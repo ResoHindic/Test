ab7e43f3aafe4436dceeb216328e3a128a7932e4
unknown <resad.hindic@gmail.com>
2015-03-10 23:30:47 -0600

    web application

---
+++ CanvasThingy/WebApplication1/WebApplication1/Course.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class Course
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string code { get; set; }
+        public string description { get; set; }
+        public DateTime? startAt { get; set; }
+        public DateTime? endAt { get; set; }
+        public DateTime? createdAt { get; set; }
+        public DateTime? updatedAt { get; set; }
+    }
+}
---
+++ CanvasThingy/WebApplication1/WebApplication1/FirstClass.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Web;
+
+namespace WebApplication1 
+{
+    public class FirstClass
+    {
+        public void MakeRequest(string request)
+        {
+            Course course = new Course();
+            string message = "";
+
+            //TODO: figure out how to make curl calls 
+
+            WebClient webClient = new WebClient();
+            dynamic result =
+                jsonConvert.Parse(
+                    webClient.DownloadString(""));
+            Console.WriteLine(result.response.user.firstName);
+        }
+
+
+
+
+        public string CreateRequest()
+        {
+            string request = "";
+
+
+            return request;
+        }
+
+        public void GetToken()
+        {
+            try
+            {
+               // TODO: figure out how to make curl calls
+                HttpWebRequest request = WebRequest.Create("") as HttpWebRequest;
+                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                {
+
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+    }
+
+
+}
